a1166fd1aa4b50f590c0134c24a7c15bb0013854
MiguelIbrahimE <miguelibrahim8@gmail.com>
2025-05-12 19:54:16 +0200

    buy me a coffee

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,7 +1,12 @@
+// fe/src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import router from './router'; // the createBrowserRouter we exported
+import router from './router'; // Your createBrowserRouter instance
+
+// Import global CSS files here
+import './index.css'; // If you have a global index.css for resets, base styles, etc.
+import './global-css/navbar.css'; // <--- YOUR GLOBAL NAVBAR STYLES
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
